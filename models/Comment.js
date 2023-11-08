85b04a8100bbdfa4d66db11cf20e882810b15a33
elmfer <elmfer11@outlook.com>
2023-11-07 19:58:33 -0600

    added comment model

---
+++ models/Comment.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model {};
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    author_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      },
+      onDelete: 'cascade'
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    updatedAt: false,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
